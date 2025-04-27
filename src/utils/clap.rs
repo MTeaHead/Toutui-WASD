d0dad89d99509e5ae98e5c600f48ff38a0d7da19
AlbanDAVID <alban.dav@live.fr>
2025-04-27 16:29:05 +0200

    remove redundant version print

--- src/utils/clap.rs
+++ src/utils/clap.rs
@@ -1,12 +1,7 @@
 use clap::Command;
 
 pub fn clap() {
-    let matches = Command::new("toutui")
+    let _matches = Command::new("toutui")
         .version(env!("CARGO_PKG_VERSION")) 
         .get_matches();
-
-    if matches.args_present() {
-        println!("{}", env!("CARGO_PKG_VERSION"));
-        return;
-    }
 }
